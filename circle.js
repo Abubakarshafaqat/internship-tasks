2569ba3906c6d12f7b164e53eb71e31c8c186adb
Abubakae <abubakarshafaqat696@gmail.com>
2024-10-15 00:06:30 +0500

    tsks of day 1

---
+++ circle.js
@@ -0,0 +1,2 @@
+const user = prompt("Enter the radious of circle")
+alert("area of a circle " + (user ** 2) * Math.PI)
---
+++ greeting.js
@@ -0,0 +1,3 @@
+const userName = prompt("What is your name :")
+const age = prompt("What is your age :")
+alert(`Hy, Good morning   ${userName}  you are ${age} years old`)
